e81b30844ee2648d62c1b94a153e9282425946a8
Lucas Romero <luqasn@gmail.com>
2022-12-31 14:23:44 +0100

    update stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ helm dependency update argocd
 
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 helm upgrade --install argocd argocd -n argocd --create-namespace --wait --timeout 120s --values globalValues.yaml
-kubectl patch secret -n argocd argocd-secret -p '{"stringData": { "admin.password": "'$(htpasswd -bnBC 10 "" ${adminpassword} | tr -d ':\n')'"}}'
+k3s kubectl patch secret -n argocd argocd-secret -p '{"stringData": { "admin.password": "'$(htpasswd -bnBC 10 "" ${adminpassword} | tr -d ':\n')'"}}'
 
 echo "ArgoCD admin password has been set to: ${adminpassword}"
 
